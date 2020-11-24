6dbcb2ea2b837b6d84c11f91ce152148d95b3449
Morgan-Wilkinson <46545893+Morgan-Wilkinson@users.noreply.github.com>
2020-11-23 20:22:40 -0500

    Initial commit

---
+++ Assets/Scripts/Tile.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Tile : MonoBehaviour
+{
+   private static Tile selected; // 1 
+    private SpriteRenderer Renderer; // 2
+    public Vector2Int Position;
+    
+    private void Start() // 3
+    {
+        Renderer = GetComponent<SpriteRenderer>();
+    }
+
+    public void Select() // 4
+    {
+        Renderer.color = Color.grey;
+    }
+
+    public void Unselect() // 5 
+    {
+        Renderer.color = Color.white;
+    }
+
+    private void OnMouseDown() //6
+    {
+        if (selected != null)
+        {
+            selected.Unselect();
+        }
+        selected = this;
+        Select();
+    }
+}
